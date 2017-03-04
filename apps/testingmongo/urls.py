b9a12ae771939308794a2edecd8d4626ba4dd01d
subhambiswajit <digu35@gmail.com>
2017-03-04 16:51:10 +0530

    views changed

--- apps/testingmongo/urls.py
+++ apps/testingmongo/urls.py
@@ -3,5 +3,6 @@ from apps.testingmongo import views
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', views.home, name='home'),
+    url(r'graphs^$', views.graphs, name='graphs'),
     # url(r'^blog/', include('blog.urls')),
 )
--- apps/testingmongo/views.py
+++ apps/testingmongo/views.py
@@ -7,8 +7,25 @@ from django.contrib.auth import authenticate
 from django.http import *
 from django.contrib.auth import login,  logout
 import datetime
+import json
 # Create your views here.
 
 
 def home(request):
 	return render (request,'index.html')
+
+
+def graphs(request):
+	tweets_data_path = 'anusha.txt'
+	tweets_data = []
+	tweets_file = open(tweets_data_path, "r")
+	for line in tweets_file:
+	    try:
+	        tweet = json.loads(line)
+	        tweets_data.append(tweet)
+	    except:
+	        continue
+	print len(tweets_data)
+	for total in range(len(tweets_data)):
+		print tweets_data[total]['user']['location']
+	return render (request, 'index.html')
